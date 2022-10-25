d9c689d759db504fffebbf1485291c83514075d9
patricija <patricija@danesjenovdan.si>
2022-10-25 16:14:07 +0200

    remove x-frame-options header

--- odprti_racuni/settings.py
+++ odprti_racuni/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'odprti_racuni.urls'
