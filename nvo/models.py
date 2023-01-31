7046573fa9d716526e7eb4055bad6707755fd45e
Tomaž kunst <cofek0@gmail.com>
2023-01-31 13:40:37 +0100

    fixed typos and add conto to finance

--- nvo/models.py
+++ nvo/models.py
@@ -28,7 +28,7 @@ def validate_image_extension(value):
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.jpg', '.jpeg', '.png']
     if not ext.lower() in valid_extensions:
-        raise ValidationError('Format slike ni med podprtimi formati. Podprti formati so jpg in png.')
+        raise ValidationError('Format slike ni veljaven ali pa je slika poškodovana.')
 
 # MODELS
 
